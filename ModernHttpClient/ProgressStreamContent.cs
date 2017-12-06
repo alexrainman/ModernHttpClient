996c5dc63da3d218727e046efa0f33d0596ca721
Alexander Reyes <alexrainman1975@gmail.com>
2017-12-06 17:06:15 -0600

    Version 2.4.3

---
+++ ModernHttpClient/ProgressStreamContent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace ModernHttpClient
+{
+    public class ProgressStreamContent : StreamContent
+    {
+        const string wrongVersion = "You're referencing the Portable version in your App - you need to reference the platform (iOS/Android) version";
+
+        ProgressStreamContent(Stream stream) : base(stream)
+        {
+            throw new Exception(wrongVersion);
+        }
+
+        ProgressStreamContent(Stream stream, int bufferSize) : base(stream, bufferSize)
+        {
+            throw new Exception(wrongVersion);
+        }
+
+        public ProgressDelegate Progress
+        {
+            get { throw new Exception(wrongVersion); }
+            set { throw new Exception(wrongVersion); }
+        }
+    }
+}
